6af62305caf60dbeb39db828528697b669f13f65
joshwilcken <joshuawilcken@Joshuas-MacBook-Pro.local>
2017-07-14 11:53:08 -0600

    adding updates for filler and slider

--- public/app/controllers/profileCtrl/profileCtrl.js
+++ public/app/controllers/profileCtrl/profileCtrl.js
@@ -1,10 +1,26 @@
 angular.module('app').controller('profileCtrl', function ($scope, profileSvc) {
+
+
             profileSvc.getData().then((resp) => {
                 $scope.currentData = resp.data
+                $scope.count += 1
+                if($scope.count === 2){
+                    $scope.dataLoad = !$scope.dataLoad
+                    $scope.dataLoader = !$scope.dataLoader
+                }
             })
 
             profileSvc.getTotals().then((resp)=> {
                 $scope.totalsData = resp.data
-                console.log($scope.totalsData)
+                $scope.count += 1
+                if($scope.count === 2){
+                    $scope.dataLoad = !$scope.dataLoad
+                    $scope.dataLoader = !$scope.dataLoader
+                }
             })
+
+            $scope.count = 0
+            console.log($scope.count)
+            $scope.dataLoad = false;
+            $scope.dataLoader = true;
 })
